1a886d63fee449106546453a3d29565aac7331c7
Vardaan <vardaan119@gmail.com>
2018-06-01 22:37:24 -0400

    setup redux-form submission - almost

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -12,20 +12,41 @@ class PostsNew extends Component {
     // onFocus={field.input.onFocus} etc etc saves a lot of time!
     // Also, any props given to the Field JSX component are passed onto the field object
     // i.e. field.label in our case
+    // also the error object from validate is present here inside the meta object
+    // so field.meta.error
+    const { meta: { touched, error } } = field // nested destructuring. touched and error pulled out from meta object, which is pulled out of field object
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
     return (
-      <div className="form-group">
+      <div className={className}>
         <label> {field.label} </label>
         <input className="form-control"
           type="text"
           {...field.input}
         />
+        <div className={"text-help"}>
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
 
+  // form submission logic
+  onSubmit(values) {
+    console.log(values)
+  }
+
   render() {
+    // redux form passes this prop to this component
+    const { handleSubmit } = this.props
+    // form onSubmit, take the handleSubmit function and give it a function that runs the submission logic
+    // if we just did onSubmit, we wouldn't have gone through redux-forms validation
+    // process and we won't have any of the values from the field
+    // handleSubmit is the connection between all the things we need from redux-form
+    // and our own function that handles the submission logic
+    // the handleSubmit function also gives our custom function the values object
+    // so we can use the values submitted by the user
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           name="title"
           component={this.renderField}
@@ -41,6 +62,7 @@ class PostsNew extends Component {
           component={this.renderField}
           label="Content"
         />
+        <button type="submit" className="btn btn-primary"> Submit </button>
       </form>
     );
   }
